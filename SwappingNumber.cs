cf3eab750d8fedbcf2ed740217612ec0b780f5d1
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:47:29 +0530

    swapping number prblm

---
+++ SwappingNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class SwappingNumber
+    {
+        public static void CheckSwap()
+        {
+            int a = 10;
+            int b = 20;
+            int temp;
+
+            Console.WriteLine("Before swapping value of a={0},b={1}", a, b);
+
+            temp = a;
+            a = b;
+            b= temp;
+
+            Console.WriteLine("After the swapping value of a={0},b={1}", a, b);
+        }
+    }
+}
