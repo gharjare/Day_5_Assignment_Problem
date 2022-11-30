f244cc5898437c85705617dbe9d54068e921e8a1
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:34:47 +0530

    Add program file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FunctionalProgramming
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //FlipCoin.Display();
+            //LeapYear.CheckYear();
+            // PowerOfTwo.CheckPower();
+            //HarmonicNumber.CheckSeries();
+            // FactorialNumber.CheckFactor();
+            // EvenOrOdd.Dispaly();
+            //SwappingNumber.CheckSwap();
+            // VowelAndConsonant.Display();
+            //QuotientAndRemainder.CheckQuotientAndRemainder();
+            LargestNumber.CheckLargestNumber();
+
+        }
+    }
+}
