df3b803c2cfb3991a8dd8271b1bcac2f6ca9b7f8
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:44:15 +0530

    factorial number prblm

---
+++ FactorialNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class FactorialNumber
+    {
+        public static void CheckFactor()
+        {
+            int fact = 1;
+
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of" + num + "is" + fact);
+        }  
+    }
+}
