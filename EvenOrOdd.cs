8d7d433e10980a52b6cf99d9e1ee102e90d98593
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:57:50 +0530

    even odd number prblm

---
+++ EvenOrOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class EvenOrOdd
+    {
+
+        public static void Dispaly()
+        {
+            int remainder = 0;
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            remainder = num % 2;
+            if (remainder == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
