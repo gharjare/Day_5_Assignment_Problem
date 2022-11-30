5d278f778da04e2bdb8101b3e4b67611f00ddc1e
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:39:09 +0530

    leapyear prblm

---
+++ LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class LeapYear
+    {
+        public static void CheckYear()
+        {
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("year is leap ");
+            }
+            else
+            {
+                Console.WriteLine("year is not leap");
+            }
+        }
+    }
+}
