e01edd2123aee96cdea0fece0b2c5475f3c9a36e
gharjare <gharjarechetan@gmail.com>
2022-11-30 18:42:17 +0530

    harmonic number prblm

---
+++ HarmonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class HarmonicNumber
+    {
+        public static void CheckSeries()
+        {
+           
+            double sum = 0.0;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Calculate the sum of harmonic series");
+            Console.WriteLine("Enter the value of n");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine("1/{0}+", i);
+                sum += 1 / (float)i;
+            }
+            Console.WriteLine("\nsum of series of {0} terms: {1} \n", num, sum);
+
+        }
+    }
+}
